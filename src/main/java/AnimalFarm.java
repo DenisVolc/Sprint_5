40613116b7ed7ace5bda43280dc5cd093af7542c
Denis Volchenko <volchenko.di@infotek.ru>
2024-01-15 18:04:59 +0300

    Сделал 1 и 3 задания Осталось 2 и 4

--- src/main/java/AnimalFarm.java
+++ src/main/java/AnimalFarm.java
@@ -7,12 +7,41 @@ public class AnimalFarm {
     public AnimalFarm(ArrayList<String> farmAnimals) {
         this.farmAnimals = farmAnimals;
     }
-    public HashMap<String,Integer> countedAnimals(ArrayList<String> farmAnimals){
-        HashMap<String, String> animalsAndItNames = new HashMap<>();
-        for(String animal : farmAnimals){
-            String[] animalName= animal.split(" ");
-            animalsAndItNames.put(animalName[0],animalName[1]);
+
+    public HashMap<String,Integer> countedAnimals(ArrayList<String> farmAnimals){ // Задание №1
+        HashMap<String, Integer> resultCountedAnimals = new HashMap<>();
+        try {
+            //        HashMap<String, String> animalsAndItNames = new HashMap<>();
+            for (String animal : farmAnimals) {
+                String[] animalName = animal.split(" ");
+//            animalsAndItNames.put(animalName[0],animalName[1]);
+                String animalNameTUC = animalName[0].toUpperCase();
+                Animal.valueOf(animalNameTUC);// тригер для try/catch
+                if (!resultCountedAnimals.containsKey(animalName[0])) {
+                    resultCountedAnimals.put(animalName[0], 1);
+                } else {
+                    Integer animalsCount = resultCountedAnimals.get(animalName[1] + 1);
+                    resultCountedAnimals.put(animalName[0], animalsCount);
+                }
+            }
+        }catch(IllegalArgumentException exception){
+            System.out.println("Please correct string [Здесь вывести полностью ошибочную строку]. Incorrect input data.");
         }
-        return;
+        return resultCountedAnimals;
+    }
+    public void addAnimal(Animal animalSpecies, String animalName){//Задание №3.1
+        farmAnimals.add( animalSpecies.name() + " " + animalName );
+    }
+
+    public void addAnimal(Animal animalSpecies){//Задание №3.2
+        farmAnimals.add(animalSpecies.name() + " N");
+    }
+
+    public void addAnimal(String animalName){//Задание №3.2
+        farmAnimals.add(Animal.NOT_DEFINED.name() + " " + animalName);
+    }
+    @Override
+    public String toString(){ //Задание №4
+        return "Вид_животного Имя_животного";
     }
 }
